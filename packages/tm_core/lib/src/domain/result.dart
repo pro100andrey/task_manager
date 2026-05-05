9156980a990a7b4a4229d48479005cf878972468
Andrii Ivanov <this.andrey@gmail.com>
2026-05-05 12:27:36 +0300

    refactor(tm_core): reorganize Result class and move fold method to extension for improved readability

--- packages/tm_core/lib/src/domain/result.dart
+++ packages/tm_core/lib/src/domain/result.dart
@@ -3,17 +3,6 @@ sealed class Result<T, E> {
 
   bool get isSuccess => this is Success<T, E>;
   bool get isFailure => this is Failure<T, E>;
-
-  R fold<R>({
-    required R Function(T value) onSuccess,
-    required R Function(E error) onFailure,
-  }) {
-    final result = this;
-    if (result is Success<T, E>) {
-      return onSuccess(result.value);
-    }
-    return onFailure((result as Failure<T, E>).error);
-  }
 }
 
 final class Success<T, E> extends Result<T, E> {
@@ -26,4 +15,32 @@ final class Failure<T, E> extends Result<T, E> {
   const Failure(this.error);
 
   final E error;
+}
+
+extension ResultX<T, E> on Result<T, E> {
+  T? get value => switch (this) {
+    Success(:final value) => value,
+    Failure() => null,
+  };
+
+  E? get error => switch (this) {
+    Failure(:final error) => error,
+    Success() => null,
+  };
+
+  Result<U, E> map<U>(U Function(T) f) => switch (this) {
+    Success(:final value) => Success(f(value)),
+    Failure(:final error) => Failure(error),
+  };
+
+  R fold<R>({
+    required R Function(T value) onSuccess,
+    required R Function(E error) onFailure,
+  }) {
+    final result = this;
+    if (result is Success<T, E>) {
+      return onSuccess(result.value);
+    }
+    return onFailure((result as Failure<T, E>).error);
+  }
 }
