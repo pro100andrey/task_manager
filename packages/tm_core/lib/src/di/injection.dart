0f131e3261e13ad6c813fe420388dee8114b4ecb
Andrii Ivanov <this.andrey@gmail.com>
2026-05-06 01:17:17 +0300

    feat: streamline logging configuration initialization in dependency injection

--- packages/tm_core/lib/src/di/injection.dart
+++ packages/tm_core/lib/src/di/injection.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:logging/logging.dart';
 
+import '../../tm_core.dart';
 import '../adapters/events/ordered_domain_event_bus_impl.dart';
 import '../application/ports/domain_event_bus.dart';
 import 'injection.config.dart';
@@ -18,10 +20,9 @@ Future<void> configureTmCoreDependencies({
 }) async {
   $initTmCore(GetIt.I, environment: environment);
 
-  //  if (GetIt.I.rootLevel != null) {
-  //   Logger.root.level = loggingConfig.rootLevel;
-  // }
-
+  final loggingConfig = GetIt.I<TracingLoggingConfig>();
+  Logger.root.level = loggingConfig.rootLevel;
+  Logger.root.onRecord.listen(loggingConfig.onRecord);
 
   if (useOrderedBus) {
     GetIt.I
